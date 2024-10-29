1facc561d66aa00d8a25816d51f32ee77498217b
oasjs <oasjatoba1@gmail.com>
2024-10-29 07:14:01 -0300

    Add some initial tests

---
+++ scripts/common.js
@@ -0,0 +1,16 @@
+function loadComponent(target, componentName) {
+  fetch(`/src/sc-components/components/${componentName}.html`)
+    .then((response) => response.text())
+    .then((html) => {
+      target.innerHTML = html;
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const components = document.querySelectorAll(".component");
+
+  components.forEach((component) => {
+    const componentName = component.getAttribute("name");
+    loadComponent(component, componentName);
+  });
+});
---
+++ scripts/moodleface.js
@@ -0,0 +1,37 @@
+/* Configuração do Moodleface. */
+
+const main = document.querySelector("main");
+fetch("sc-components/img/moodleface/moodleface.svg")
+  .then((response) => response.text())
+  .then((svg) => {
+    main.insertAdjacentHTML("afterbegin", svg);
+  })
+  .then(() => {
+    for (let i = 1; i <= 3; i++) {
+      const un = main.querySelector(`#un${i}`);
+      un.style.cursor = "pointer";
+      un.addEventListener("click", () => {
+        window.open(`/src/un${i}.html`);
+      });
+    }
+
+    for (let i = 1; i <= 3; i++) {
+      const questions = main.querySelector(`#questions-${i}`);
+      questions.style.cursor = "pointer";
+    }
+
+    const about = main.querySelector("#about");
+    about.style.cursor = "pointer";
+    main.querySelector("#about").addEventListener("click", () => {
+      window.open("/src/sobre.html");
+    });
+
+    const booklet = main.querySelector("#booklet");
+    booklet.style.cursor = "pointer";
+
+    const guide = main.querySelector("#guide");
+    guide.style.cursor = "pointer";
+
+    const media = main.querySelector("#media");
+    media.style.cursor = "pointer";
+  });
