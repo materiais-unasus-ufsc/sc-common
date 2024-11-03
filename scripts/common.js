7fd7d5d846f4593e3b29b445c24d7ee589a3e2f5
oasjs <oasjatoba1@gmail.com>
2024-11-03 05:13:02 -0300

    Too much stuff, shoul've commited better

--- scripts/common.js
+++ scripts/common.js
@@ -1,3 +1,9 @@
+/**
+ * @summary Loads a static component into the target element.
+ *
+ * @param {*} target
+ * @param {*} componentName
+ */
 function loadComponent(target, componentName) {
   fetch(`sc-common/components/${componentName}.html`)
     .then((response) => response.text())
@@ -6,6 +12,9 @@ function loadComponent(target, componentName) {
     });
 }
 
+/**
+ * @summary Fetches and inserts the component component into the target element.
+ */
 function loadCallout(target, calloutType) {
   fetch(`sc-common/components/callout-${calloutType}.html`)
     .then((response) => response.text())
@@ -13,27 +22,70 @@ function loadCallout(target, calloutType) {
       target.innerHTML = html;
     })
     .then(() => {
+      if (calloutType == "glossary") {
+        target.querySelector("h6").innerText = target.getAttribute("title");
+      }
       target.querySelector("p").innerHTML = target.getAttribute("text");
     });
 }
 
+function loadFigure(module, fileName) {
+  if (fileName == "") {
+    console.error("Tried to load a figure with no name!");
+    return;
+  }
+
+  fetch(`img/${module}/${fileName}.svg`)
+    .then((response) => response.text())
+    .then((figureText) => {
+      const parser = new DOMParser();
+      let figure = parser
+        .parseFromString(figureText, "text/html")
+        .querySelector("svg");
+      figure.classList.add("d-none","d-lg-block");
+
+      let container = document.getElementById(fileName);
+      container.insertAdjacentElement("afterbegin", figure);
+    });
+}
+
+function loadAllFigures() {
+  const module = document.querySelector("main").id;
+  document.querySelectorAll("figure").forEach((element) => {
+    loadFigure(module, element.id);
+  });
+}
+
+function toggleVisibility(targetSelector) {
+  let target = document.querySelector(targetSelector);
+  if (target.style.display == "none") {
+    target.style.display = "block";
+  } else {
+    target.style.display = "none";
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
+  loadAllFigures();
+
+  /* Custom static component insertion */
   const components = document.querySelectorAll(".component");
   components.forEach((component) => {
     const componentName = component.getAttribute("name");
     loadComponent(component, componentName);
   });
 
+  /* Custom Callout component insertion */
   const callouts = document.querySelectorAll(".callout");
   callouts.forEach((callout) => {
     const calloutType = callout.getAttribute("type");
     loadCallout(callout, calloutType);
   });
 
+  /* Bootstrap Popover component activation */
   const popoverTriggerList = document.querySelectorAll(
     '[data-bs-toggle="popover"]'
   );
-
   popoverTriggerList.forEach((popoverTriggerEl) => {
     const popover = new bootstrap.Popover(popoverTriggerEl, {
       html: true,
@@ -45,7 +97,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     popoverTriggerEl.addEventListener("focusout", () => {
       const _this = popoverTriggerEl;
-      if (!document.querySelector(".popover:hover")) {
+      if (!document.querySelector(".popover:focus")) {
         popover.hide();
       } else {
         document
