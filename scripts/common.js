4d2c1a6306b7dbc00c38333d415150d5ac982b17
oasjs <oasjatoba1@gmail.com>
2024-11-13 06:47:27 -0300

    Move functions arround and implement automatic video insertion

--- scripts/common.js
+++ scripts/common.js
@@ -12,6 +12,60 @@ function loadComponent(target, componentName) {
     });
 }
 
+function loadComponents() {
+  const components = document.querySelectorAll(".component");
+  components.forEach((component) => {
+    const componentName = component.getAttribute("name");
+    loadComponent(component, componentName);
+  });
+}
+
+function loadVideos() {
+  const videos = document.querySelectorAll(".video");
+  videos.forEach((video) => {
+    fetch("sc-common/img/common/video-cover.svg")
+      .then((response) => response.text())
+      .then((svgText) => {
+        const parser = new DOMParser();
+        video.appendChild(
+          parser.parseFromString(svgText, "text/html").querySelector("svg")
+        );
+
+        // Update cover title with provided value in the title attribute
+        const spans = video.querySelector("text").childNodes;
+        spans.forEach((span) => (span.innerHTML = ""));
+        const titleWords = video.getAttribute("title").split(" ");
+        const quocient = Math.ceil(titleWords.length / 4);
+        for (let i = 0; i < titleWords.length; i++) {
+          console.log(titleWords);
+          spans[Math.floor(i / quocient)].innerHTML += `${titleWords[i]} `;
+        }
+
+        // Add video replacement logic on click over the video container
+        const container = video.querySelector("#video-cover-container");
+        container.style.cursor = "pointer";
+        const iframe = document.createElement("iframe");
+        const att = {
+          width: window.getComputedStyle(container.querySelector("rect")).width,
+          height: window.getComputedStyle(container.querySelector("rect"))
+            .height,
+          src: video.getAttribute("src"),
+          title: video.getAttribute("title"),
+          allow:
+            "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share",
+          referrerpolicy: "strict-origin-when-cross-origin",
+          allowfullscreen: "",
+        };
+        for (const [key, value] of Object.entries(att)) {
+          iframe.setAttribute(key, value);
+        }
+        container.addEventListener("click", () => {
+          container.replaceChildren(iframe);
+        });
+      });
+  });
+}
+
 /**
  * @summary Fetches and inserts a callout component into the target element.
  */
@@ -27,6 +81,14 @@ function loadCallout(target, calloutType) {
       }
       target.querySelector("p").innerHTML = target.getAttribute("text");
     });
+}
+
+function loadCallouts() {
+  const callouts = document.querySelectorAll(".callout");
+  callouts.forEach((callout) => {
+    const calloutType = callout.getAttribute("type");
+    loadCallout(callout, calloutType);
+  });
 }
 
 function loadFigure(module, fileName) {
@@ -46,47 +108,44 @@ function loadFigure(module, fileName) {
         console.error(`Figure ${fileName} is NULL!`);
         return;
       }
-      figure.classList.add("d-none","d-lg-block");
+      figure.classList.add("d-none", "d-lg-block");
 
       let container = document.getElementById(fileName);
       container.insertAdjacentElement("afterbegin", figure);
     });
 }
 
-function loadAllFigures() {
+function loadFigures() {
   const module = document.querySelector("main").id;
   document.querySelectorAll("figure").forEach((element) => {
     loadFigure(module, element.id);
   });
 }
 
+function hideElement(element) {
+  if (!element.hasAttribute("originalDisplay")) {
+    element.setAttribute("originalDisplay", element.style.display);
+  }
+  element.style.display = "none";
+}
+
+function showElement(element) {
+  element.style.display = element.getAttribute("originalDisplay");
+}
+
 function toggleVisibility(targetSelector) {
   let target = document.querySelector(targetSelector);
   if (target.style.display == "none") {
-    target.style.display = "block";
+    showElement(target);
   } else {
-    target.style.display = "none";
+    hideElement(target);
   }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  loadAllFigures();
-
-  /* Custom static component insertion */
-  const components = document.querySelectorAll(".component");
-  components.forEach((component) => {
-    const componentName = component.getAttribute("name");
-    loadComponent(component, componentName);
-  });
-
-  /* Custom Callout component insertion */
-  const callouts = document.querySelectorAll(".callout");
-  callouts.forEach((callout) => {
-    const calloutType = callout.getAttribute("type");
-    loadCallout(callout, calloutType);
-  });
-
-  /* Bootstrap Popover component activation */
+/**
+ * @brief Setup for Bootstrap popover components.
+ */
+function setUpPopovers() {
   const popoverTriggerList = document.querySelectorAll(
     '[data-bs-toggle="popover"]'
   );
@@ -113,4 +172,12 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadFigures();
+  loadVideos();
+  loadComponents();
+  loadCallouts();
+  setUpPopovers();
 });
